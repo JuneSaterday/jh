5a70d4469b41fb4456e76a0d11253de87859876d
张伟 <nico.zhang@exclouds.com.cn>
2019-04-22 17:59:45 +0800

    4/22

--- src/main/java/com/worldex/util/HTTPUtil.java
+++ src/main/java/com/worldex/util/HTTPUtil.java
@@ -1,5 +1,9 @@
 package com.worldex.util;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 /**
  * @Author: zhangwei
  * @Date: Created in 2019 04 22 下午 4:58
@@ -7,11 +11,16 @@ package com.worldex.util;
  */
 public class HTTPUtil {
 
+    private String userUrl = "https://ebusiness.cargofe.com/api/AccountAuth.csfe";
 
-    private String getSessionId(){
-        Httpc
+    private String getSessionId(String userUrl){
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(userUrl);
+
 
         return null;
     }
+
 
 }
